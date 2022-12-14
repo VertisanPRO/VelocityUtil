67dd4a66c6855547873ceb3bf065cbbd1cfd5c15
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2022-12-14 14:46:47 +0200

    Update VelocityUtil.java

--- src/main/java/com/gigabait/velocityutil/VelocityUtil.java
+++ src/main/java/com/gigabait/velocityutil/VelocityUtil.java
@@ -19,7 +19,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "velocityutil",
         name = "VelocityUtil",
-        version = "1.0.2",
+        version = "1.0.1",
         description = "Velocity Util",
         url = "vertisanpro.com",
         authors = {"GIGABAIT"}
